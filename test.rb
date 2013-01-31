961e5da2fc84a28772e7f69d420b7d8f190581cb
Jason Whitehorn <jason.whitehorn@gmail.com>
2013-01-31 03:14:05 +0000

    go from high to low

--- test.rb
+++ test.rb
@@ -3,8 +3,8 @@ require 'pi_piper'
 PiPiper::Spi.begin do |spi|
   spi.clock(0)
   (0..255).each do |i|
-    puts i
-    spi.write 0x00, 0x01, 0x01, i
+    puts 255-i
+    spi.write 0x00, 0x01, 0x01, 255-i
     sleep 1
   end
 end
