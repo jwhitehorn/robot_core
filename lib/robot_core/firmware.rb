ed4a55de50976ac8b933f2d912f34d6236bed0ef
Jason Whitehorn <jason.whitehorn@gmail.com>
2013-02-25 03:41:40 +0000

    more thoughts on what the firmware class should do

--- lib/robot_core/firmware.rb
+++ lib/robot_core/firmware.rb
@@ -1,5 +1,14 @@
+require 'pi_piper'
+
 module RobotCore
   class Firmware
+
+    def send_command(op_code, options)
+      data = [0x00, RobotCore::ByteCode.for_name(op_code)].merge options
+      PiPiper::Spi.begin
+        write data
+      end
+    end
 
   end
 end
